84f0f180e6855a8dd454c58ce2f1a3c08969743d
Filipe Correia <filipe.lp.correia@gmail.com>
2019-10-24 23:22:41 +0100

    Fixes tests

--- test/controllers/fire_controller_test.rb
+++ test/controllers/fire_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class FireControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get fire_index_url
+    get fires_url
     assert_response :success
   end
 end
