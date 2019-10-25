79c83e80089bbed860d76eb96d50a14e26f1f2bf
Filipe Correia <laginha87@users.noreply.github.com>
2019-10-25 21:03:09 +0100

    Standard test (#2)
    * Tweak redis config
    
    * Fix up workflow
    
    * Trying again

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-Rails.application.config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
-Rails.config.action_controller.perform_caching = true
+Rails.application.configure do
+    config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+    config.action_controller.perform_caching = true
+end
